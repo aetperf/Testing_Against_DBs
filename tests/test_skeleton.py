036028ff7ee9761e3e38d201e0702606c490d378
djfrancesco <pacullfrancois@gmail.com>
2022-10-07 09:26:03 +0200

    bug fix

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -3,8 +3,8 @@ from sqlalchemy import create_engine
 
 from dbtests.skeleton import fib, main
 
-__author__ = "djfrancesco"
-__copyright__ = "djfrancesco"
+__author__ = "François Pacull"
+__copyright__ = "Architecture & Performance"
 __license__ = "MIT"
 
 
@@ -25,7 +25,9 @@ def test_main(capsys):
     captured = capsys.readouterr()
     assert "The 7-th Fibonacci number is 13" in captured.out
 
+
 def test_connection():
+    """Test connection and write permissions with SQL Server."""
     username = "SA"
     password = "Admin123"
     server = "localhost"
@@ -35,4 +37,29 @@ def test_connection():
 
     engine = create_engine(connect_string)
     conn = engine.connect()
+
+    query_1 = """
+        CREATE TABLE recipes (
+          recipe_id INT NOT NULL,
+          recipe_name VARCHAR(30) NOT NULL,
+          PRIMARY KEY (recipe_id),
+          UNIQUE (recipe_name)
+        );"""
+    _ = conn.execute(query_1)
+
+    query_2 = """
+        INSERT INTO recipes 
+            (recipe_id, recipe_name) 
+        VALUES 
+            (1, 'Tacos'),
+            (2, 'Tomato Soup'),
+            (3, 'Grilled Cheese');"""
+    _ = conn.execute(query_2)
+
+    query_3 = """
+        SELECT COUNT(*) FROM recipes"""
+    result = conn.execute(query_3).fetchone()[0]
+
     conn.close()
+
+    assert result == 3
